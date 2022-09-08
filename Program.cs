71437817bb7cd4d9680c37ff115d7962ad936b2a
logan.thomas <logan.thomas@forsta.com>
2022-09-07 19:42:37 -0700

    started connecting the database to the api through docker-compose

--- Program.cs
+++ Program.cs
@@ -47,11 +47,11 @@ builder.Services.AddHttpContextAccessor();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+// if (app.Environment.IsDevelopment())
+// {
+app.UseSwagger();
+app.UseSwaggerUI();
+// }
 
 app.UseHttpsRedirection();
 
